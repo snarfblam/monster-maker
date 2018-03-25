766066ea98e0b71c871644ffe851794e147e906e
Tom H <snarfblam@gmail.com>
2018-03-24 22:16:08 -0400

    MySql schema/seed

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS monsters_db;
+USE monsters_db;
+
+CREATE TABLE IF NOT EXISTS `monsters` (
+    id INTEGER AUTO_INCREMENT,
+    partyId INTEGER NOT NULL,
+    body INTEGER NOT NULL,
+    head INTEGER NOT NULL,
+    eyes INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    active BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS `parties` (
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO `monsters_db`.`monsters` (
+    `partyId`, `body`, `head`, `eyes`, `name`, `active`
+) VALUES (
+    '1', '1', '1', '1', 'Birdo', '1'
+);
+INSERT INTO `monsters_db`.`monsters` (
+    `partyId`, `body`, `head`, `eyes`, `name`, `active`
+) VALUES (
+    '1', '2', '2', '2', 'Crabby', '1'
+);
+INSERT INTO `monsters_db`.`monsters` (
+    `partyId`, `body`, `head`, `eyes`, `name`, `active`
+) VALUES (
+    '1', '3', '3', '3', 'Sir Axalot', '1'
+);
+INSERT INTO `monsters_db`.`monsters` (
+    `partyId`, `body`, `head`, `eyes`, `name`, `active`
+) VALUES (
+    '1', '4', '4', '4', 'Gollem', '1'
+);
+
+INSERT INTO `monsters_db`.`parties` (
+    `id`, `name`
+) VALUES (
+    '1', 'Sample party'
+);
